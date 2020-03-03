c01bdbaa4ce64df4052a38943f208f2576285c2e
maxmyslivets <by.mikky1206@gmail.com>
2020-03-04 00:34:00 +0300

    fix round

--- lib/BLtoXY_gaus_kruger.py
+++ lib/BLtoXY_gaus_kruger.py
@@ -8,15 +8,15 @@ def BLtoXY_gaus_kruger(B, L):
     """Вычисление прямоугольных координат Гаусса-Крюгера по геодезическим"""
     
     print('Вычисляем радиус кривизны первого вертикала:')
-    N = round((6399698.902-(21562.267-(108.973-0.612*cos(dms_to_rad(B))**2)*cos(dms_to_rad(B))**2)*cos(dms_to_rad(B))**2), 4)
+    N = round((6399698.902-(21562.267-(108.973-0.612*cos(dms_to_rad(B))**2)*cos(dms_to_rad(B))**2)*cos(dms_to_rad(B))**2), 3)
     print('N =', N)
 
     print('Вычисляем коэффициенты a0, a3, a4, a5, a6:')
-    a0 = round((32140.404-(135.3302-(0.7092-0.0040*cos(dms_to_rad(B))**2)*cos(dms_to_rad(B))**2)*cos(dms_to_rad(B))**2), 4)
-    a3 = round(((0.3333333+0.001123*cos(dms_to_rad(B))**2)*(cos(dms_to_rad(B))**2)-0.1666667), 4)
-    a4 = round(((0.25+0.00252*cos(dms_to_rad(B))**2)*(cos(dms_to_rad(B))**2)-0.04166), 4)
-    a5 = round((0.0083-(0.1667-(0.1968+0.0040*cos(dms_to_rad(B))**2)*cos(dms_to_rad(B))**2)*cos(dms_to_rad(B))**2), 4)
-    a6 = round(((0.166*(cos(dms_to_rad(B))**2)-0.084)*cos(dms_to_rad(B))**2), 4)
+    a0 = round((32140.404-(135.3302-(0.7092-0.0040*cos(dms_to_rad(B))**2)*cos(dms_to_rad(B))**2)*cos(dms_to_rad(B))**2), 3)
+    a3 = round(((0.3333333+0.001123*cos(dms_to_rad(B))**2)*(cos(dms_to_rad(B))**2)-0.1666667), 8)
+    a4 = round(((0.25+0.00252*cos(dms_to_rad(B))**2)*(cos(dms_to_rad(B))**2)-0.04166), 8)
+    a5 = round((0.0083-(0.1667-(0.1968+0.0040*cos(dms_to_rad(B))**2)*cos(dms_to_rad(B))**2)*cos(dms_to_rad(B))**2), 8)
+    a6 = round(((0.166*(cos(dms_to_rad(B))**2)-0.084)*cos(dms_to_rad(B))**2), 8)
 
     print('a0 =', a0, '\na3 =', a3, '\na4 =', a4, '\na5 =', a5, '\na6 =', a6)
 
@@ -38,8 +38,8 @@ def BLtoXY_gaus_kruger(B, L):
     print('l =', l)
 
     print('Вычисляем прямоугольные координаты Гаусса-Крюгера:')
-    x = round((6367558.4969 * (dms_to_s(B)/206265)-(a0-(0.5+(a4+a6*l**2)*l**2)*(l**2)*N)*sin(dms_to_rad(B))*cos(dms_to_rad(B))), 4)
-    y = round(((1+(a3+a5*l**2)*l**2)*l*N*cos(dms_to_rad(B))), 4)
+    x = round((6367558.4969 * (dms_to_s(B)/206265)-(a0-(0.5+(a4+a6*l**2)*l**2)*(l**2)*N)*sin(dms_to_rad(B))*cos(dms_to_rad(B))), 3)
+    y = round(((1+(a3+a5*l**2)*l**2)*l*N*cos(dms_to_rad(B))), 3)
     print('x =', x, '\ny =', y)
 
     return [x, y]
